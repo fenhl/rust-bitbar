3ec373bb6c5fb91db3477ab947b5975a6989b5fe
Fenhl <fenhl@fenhl.net>
2019-06-23 13:52:21 +0000

    Copy readme contents to crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! This is `bitbar`, a library crate which includes helpers for writing [BitBar](https://getbitbar.com/) plugins in Rust. The main feature is the `Menu` type whose `Display` implementation generates output that conforms to the [BitBar plugin API](https://github.com/matryer/bitbar#plugin-api).
+//!
 //! # Features
 //!
 //! The following features can be enabled via Cargo:

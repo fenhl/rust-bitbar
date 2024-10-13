6fa13979c7316489a951bd75c5e0a7599e538d24
Fenhl <fenhl@fenhl.net>
2024-10-13 21:17:22 +0000

    Update release script

--- assets/release.sh
+++ assets/release.sh
@@ -1,11 +1,12 @@
-#!/usr/local/bin/zsh
+#!/usr/bin/env zsh
 
 set -e
 
+git push
 cd crate/bitbar-derive
 cargo publish
 #TODO only release cargo-bitbar if changed
-cd ../cargo-bitbar
-cargo publish
+#cd ../cargo-bitbar
+#cargo publish
 cd ../bitbar
 cargo publish
